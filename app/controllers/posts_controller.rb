4cd0061cefa2e7ac10f35811c4a7611eb8cc7c62
陶梦迪 <taomengdi99@163.com>
2017-01-02 17:40:23 +0800

    all things have done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,24 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post deleted!"
+  end
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
 
   private
 
